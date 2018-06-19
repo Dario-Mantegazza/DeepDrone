80a99b19a2e95a1b3160ef6c780b69af4bf03f0a
Dario Mantegazza <dadomante@gmail.com>
2018-06-20 00:45:44 +0200

    time info print

--- keras_crossvalidation.py
+++ keras_crossvalidation.py
@@ -76,12 +76,12 @@ def CNNMethod(batch_size, epochs, model_name, num_classes, save_dir, x_test, x_t
     vidcr_test = KerasVideoCreator(x_test=x_test, labels=y_test, preds=y_pred, title=save_dir + "/result_model_" + str(i) + "/test_result.avi")
     vidcr_test.video_plot_creator()
     dumb_metrics = dumb_regressor_result(x_test, x_train, y_test, y_train)
-    # show some plots
     plot_results_cross(history, y_pred, y_test, dumb_metrics, save_dir, i)
     return history.history, dumb_metrics
 
 
 def crossValidation(k_fold, batch_size, num_classes, epochs):
+    start_time = datetime.now()
     save_path = 'saves/' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     try:
         os.makedirs(save_path)
@@ -103,6 +103,7 @@ def crossValidation(k_fold, batch_size, num_classes, epochs):
         print('No bag files found!')
         return None
     for i in range(k_fold):  # test selection,
+        e_start_time = datetime.now()
         x_test_list = []
         x_train_list = []
 
@@ -135,7 +136,54 @@ def crossValidation(k_fold, batch_size, num_classes, epochs):
         history, dumb_results = CNNMethod(batch_size, epochs, model_name, num_classes, save_dir, x_test, x_train, y_test, y_train, i)
         history_list.append(history)
         dumb_list.append(dumb_results)
+        e_end_time = datetime.now()
+        print("")
+        print("")
+        print("----k-Fold " + str(i) + " info:")
+        print("started: " + e_start_time.strftime("%Y-%m-%d-%H-%M-%S"))
+        print("ended:   " + e_end_time.strftime("%Y-%m-%d-%H-%M-%S"))
+        h = divmod((e_end_time - e_start_time).total_seconds(), 3600)  # hours
+        m = divmod(h[1], 60)  # minutes
+        s = m[1]  # seconds
+        print("lasted:  %d hours, %d minutes, %d seconds" % (h[0], m[0], s))
+        if i < 4:
+            eta_h = divmod((k_fold - i - 1) * (e_end_time - e_start_time).total_seconds(), 3600)  # hours
+            eta_m = divmod(eta_h[1], 60)  # minutes
+            eta_s = eta_m[1]  # seconds
+            print("ETA:     %d hours, %d minutes, %d seconds" % (eta_h[0], eta_m[0], eta_s))
+        print("----")
+        print("")
+        print("")
+        with open(save_dir + "/computation_time.txt", "w+") as outfile:
+            outfile.write("----k-Fold " + str(i) + " info:")
+            outfile.write("started: " + e_start_time.strftime("%Y-%m-%d-%H-%M-%S"))
+            outfile.write("ended:   " + e_end_time.strftime("%Y-%m-%d-%H-%M-%S"))
+            outfile.write("lasted:  %d hours, %d minutes, %d seconds" % (h[0], m[0], s))
+            outfile.write("----")
+            outfile.close()
+    hist_df = pd.DataFrame(history_list)
+    pickle_path = save_dir + "/metrics_history.pickle"
+    hist_df.to_pickle(pickle_path)
+    print("history saved: " + pickle_path)
     history_data_plot_crossvalidation(history_list, dumb_list, save_dir)
+    end_time = datetime.now()
+    print("")
+    print("")
+    print("---- total computation time:")
+    print("started: " + start_time.strftime("%Y-%m-%d-%H-%M-%S"))
+    print("ended:   " + end_time.strftime("%Y-%m-%d-%H-%M-%S"))
+    h = divmod((end_time - start_time).total_seconds(), 3600)  # hours
+    m = divmod(h[1], 60)  # minutes
+    s = m[1]  # seconds
+    print("lasted:  %d hours, %d minutes, %d seconds" % (h[0], m[0], s))
+    print("----")
+    with open(save_dir + "/computation_time.txt", "w+") as outfile:
+        outfile.write("---- total computation time:")
+        outfile.write("started: " + start_time.strftime("%Y-%m-%d-%H-%M-%S"))
+        outfile.write("ended:   " + end_time.strftime("%Y-%m-%d-%H-%M-%S"))
+        outfile.write("lasted:  %d hours, %d minutes, %d seconds" % (h[0], m[0], s))
+        outfile.write("----")
+        outfile.close()
 
 
 # ------------------- Main ----------------------
@@ -143,7 +191,7 @@ def main():
     k_fold = 5
     batch_size = 64
     num_classes = 4
-    epochs = 30
+    epochs = 40
     # epochs = 2
     crossValidation(k_fold, batch_size, num_classes, epochs)
 
